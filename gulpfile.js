27e83780d8ca59dc2752e99fe0c8470a03760a92
streetcoder <proshimul@yahoo.com>
2015-09-16 05:41:56 +0600

    enable source map for compass, clean up and modify directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 // ## Globals
 
 var gulp 			= require('gulp');
-var concat 			= require('gulp-concat');
 var sass            = require('gulp-sass');
 var minifyCss       = require('gulp-minify-css');
+var sourcemaps      = require('gulp-sourcemaps');
 var autoprefix      = require('gulp-autoprefixer');
 var rename 			= require('gulp-rename');
 var jshint          = require('gulp-jshint');
 var uglify 			= require('gulp-uglify');
+var concat 			= require('gulp-concat');
 var runSequence     = require('run-sequence').use(gulp);
 var watch           = require('gulp-watch');
 var browserSync     = require('browser-sync').create();
@@ -17,34 +18,56 @@ var imagemin        = require('gulp-imagemin');
 var compass         = require('gulp-compass');
 var path            = require('path');
 
-// this task compile scss files with compass @mixin, variable to dist directory
-gulp.task('compass', function() {
-    gulp.src('assets/sass/main.scss')
-        .pipe(plumber())
+// ## tasks
+
+// dependency stylesheet i.e. bootstrap compile from bower_component
+// and copy it to dist directory with source map
+gulp.task('depsStyles', function()
+{
+    gulp.src('src/app/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/styles'));
+});
+
+// minify dependency scripts and copy to scripts directory
+gulp.task('depsScripts',function()
+{
+    return gulp.src(['bower_components/jquery/dist/jquery.js',
+        'bower_components/modernizr/modernizr.js',
+        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('dependency', ['depsStyles','depsScripts']);
+
+// it compiles all scss files for development,
+// create source maps
+// minified and transferred to dist
+// plumber used to catch error
+gulp.task('styles', function() {
+    gulp.src('src/sass/*.scss')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }}))
         .pipe(compass({
-            project: path.join(__dirname, 'assets'),
-            css: 'css',
-            sass: 'sass',
-            image: 'img',
-            cache: false
+            css: 'dist/styles',
+            sass: 'src/sass'
         }))
-        .pipe(gulp.dest('css'))
+        .pipe(sourcemaps.init())
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/styles'))
         .pipe(browserSync.stream());
 });
 
-// minify css
-gulp.task('minifyCss', function()
-{
-    return gulp.src(['assets/css/main.css'])
-        .pipe(autoprefix('last 2 versions'))
-        .pipe(rename('main.min.css'))
-        .pipe(minifyCss())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
-});
 
 gulp.task('jshint', function() {
-    gulp.src('assets/js/main.js')
+    gulp.src('src/scripts/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -53,33 +76,30 @@ gulp.task('jshint', function() {
 // jquery.js, modernizr.js, bootstrap.js
 gulp.task('scripts', ['jshint'],function()
 {
-    return gulp.src(['bower_components/jquery/dist/jquery.js',
-        'bower_components/modernizr/modernizr.js',
-        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-        'assets/js/main.js'])
+    return gulp.src(['src/scripts/main.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('images', function() {
-    return gulp.src(['assets/img/**/*'])
+    return gulp.src(['src/images/**/*'])
         .pipe(imagemin({
             progressive: true,
             interlaced: true,
             svgoPlugins: [{removeUnknownsAndDefaults: false}, {cleanupIDs: false}]
         }))
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest('dist/images'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
-    return gulp.src('assets/fonts/**/*')
+    return gulp.src('src/fonts/**/*')
         .pipe(flatten())
         .pipe(gulp.dest('dist/fonts'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['minifyCss'], function()
+gulp.task('watch', function()
 {
     browserSync.init({
         server: {
@@ -87,14 +107,13 @@ gulp.task('watch', ['minifyCss'], function()
         }
     });
 
-    gulp.watch('assets/sass/*.scss', ['compass']);
-    gulp.watch('assets/css/*.css', ['minifyCss']);
-    gulp.watch('assets/js/*.js', ['scripts']);
-    gulp.watch('assets/img/**/*', ['images']);
-    gulp.watch('assets/fonts/**/*', ['fonts']);
+    gulp.watch('src/sass/**/*.scss', ['styles']);
+    gulp.watch('src/scripts/*.js', ['scripts']);
+    gulp.watch('src/images/**/*', ['images']);
+    gulp.watch('src/fonts/**/*', ['fonts']);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', function(callback){
-    runSequence('compass', 'minifyCss', 'scripts', 'images', 'fonts', callback);
+    runSequence('dependency', 'styles', 'scripts', 'images', 'fonts', callback);
 });
